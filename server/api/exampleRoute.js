6f7bc4a573eb314d1b5cead67369580659a5f04f
Gregory Millare <gmillare4@gmail.com>
2020-07-29 12:11:32 -0500

    setup express

---
+++ server/api/exampleRoute.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+
+/* 
+    Example Routes
+    
+    router.get('/', function (req, res, next) {etc});
+
+    router.post('/', function (req, res, next) {etc});
+
+    router.put('/:puppyId', function (req, res, next) {etc});
+
+    router.delete('/:puppyId', function (req, res, next) {etc});
+
+*/
+
+module.exports = router;
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 5500;
+
+const app = express();
+
+// Logging Middleware
+app.use(morgan("dev"));
+
+// Body Parsing Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// API Routes
+app.use("/api", require("./apiRoutes"));
+
+// Static File Middleware
+app.use(express.static(path.join(__dirname, "../public")));
+
+// Send HTML: requests that don't match API Routes
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public"));
+});
+
+// 500 Error: something wrong on our part
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.err(err.stack);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
+
+// Listen and start server on PORT 5500
+app.listen(port, function () {
+  console.log("The server smiles upon you");
+  console.log("Hello");
+  console.log(`Your server, listening on port ${PORT}`);
+});
+
+module.exports = app;
